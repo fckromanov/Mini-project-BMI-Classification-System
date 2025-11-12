4a7a6664f63c3c10acee94fb2597c27f60ac7000
Romanov Timofey <romanov.t1@mail.ru>
2025-11-13 00:29:22 +0300

    Updating the condition

--- main.go
+++ main.go
@@ -23,37 +23,30 @@ func main() {
 	fmt.Printf("Индекс массы тела: %.2f\n", imt)
 	fmt.Println("")
 	fmt.Println("✅ Рекомендация:")
-	if imt < 16 {
+	switch {
+	case imt < 16:
 		category = "Выраженный дефицит массы"
-	} else if imt >= 16 && imt < 18.5 {
+		recommendation = "Больше кушайте"
+	case imt >= 16 && imt < 18.5:
 		category = "Недостаточная масса"
-	} else if imt >= 18.5 && imt < 25 {
+		recommendation = "Больше кушайте"
+	case imt >= 18.5 && imt < 25:
 		category = "Норма"
-	} else if imt >= 25 && imt < 30 {
+		recommendation = "Продолжайте поддерживать здоровый образ жизни!"
+	case imt >= 25 && imt < 30:
 		category = "Избыточная масса"
-	} else if imt >= 30 && imt < 35 {
+		recommendation = "Ходите в зал, следите за питанием"
+	case imt >= 30 && imt < 35:
 		category = "Ожирение 1 степени"
-	} else if imt >= 35 && imt < 40 {
+		recommendation = "Запишитесь к врачу"
+	case imt >= 35 && imt < 40:
 		category = "Ожирение 2 степени"
-	} else {
+		recommendation = "Запишитесь к врачу"
+	default:
 		category = "Ожирение 3 степени"
+		recommendation = "Запишитесь к врачу"
 	}
 	fmt.Printf("Категория: %s\n", category)
-	if imt < 16 {
-		recommendation = "Больше кушайте"
-	} else if imt >= 16 && imt < 18.5 {
-		recommendation = "Больше кушайте"
-	} else if imt >= 18.5 && imt < 25 {
-		recommendation = "Продолжайте поддерживать здоровый образ жизни!"
-	} else if imt >= 25 && imt < 30 {
-		recommendation = "Ходите в зал, следите за питанием"
-	} else if imt >= 30 && imt < 35 {
-		recommendation = "Запишитесь к врачу"
-	} else if imt >= 35 && imt < 40 {
-		recommendation = "Запишитесь к врачу"
-	} else {
-		recommendation = "Запишитесь к врачу"
-	}
 	fmt.Println(recommendation)
 	fmt.Println("")
 	fmt.Println("Спасибо, что пользуетесь нашим сервисом")
